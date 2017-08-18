92d1564262187702596ad1246e2f551b3a2cdfb1
Dylan Beaudette <dylan.beaudette@gmail.com>
2017-08-18 14:37:13 -0700

    better error flagging, need to work on RO2 problems

--- problem-OSDs/find-errors.sql
+++ problem-OSDs/find-errors.sql
@@ -26,9 +26,26 @@ CREATE TEMP TABLE problems AS
 SELECT series, t AS top, b as BOTTOM, t[n], b[n-1], n from missing_hz_check where t[n] != b[n-1] ;
 
 CREATE TEMP TABLE problems_by_mo AS
-SELECT mlraoffice, seriesname 
+SELECT mlraoffice, seriesname, ac
 FROM taxa 
-WHERE seriesname IN (SELECT series from problems) ORDER BY mlraoffice;
+JOIN soilweb.series_stats ON taxa.seriesname = series_stats.series
+WHERE seriesname IN (SELECT series from problems) 
+ORDER BY mlraoffice, ac DESC;
+
+
+-- 2. RO 2 specific: missing colors
+CREATE TEMP TABLE ro2_errors AS
+select ac, osd_colors.series, hzname, top, bottom 
+FROM osd_colors 
+JOIN soilweb.series_stats USING (series)
+WHERE matrix_wet_color_hue IS NULL 
+AND series IN (select seriesname from taxa where mlraoffice = 2)
+AND hzname NOT LIKE '%R%'
+AND hzname NOT LIKE '%Cr%'
+ORDER BY ac DESC, series;
 
 \copy problems TO 'problem-osds.csv' CSV HEADER
 \copy problems_by_mo TO 'problems-by-mo.csv' CSV HEADER
+\copy ro2_errors TO 'ro2_errors.csv' CSV HEADER
+
+
