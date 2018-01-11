24b32e05104b561354bcf38eabefd66af288f869
Dylan Beaudette <dylan.beaudette@gmail.com>
2018-01-11 10:45:32 -0800

    added SQL for site data and some code cleanup

--- sql/load-parsed-osd-data.sql
+++ sql/load-parsed-osd-data.sql
@@ -10,19 +10,34 @@
 -- 2016-10-04: added horizon narrative column
 --
 
+--
+-- be sure to strip non-ascii characters
+--
+-- iconv -c parsed-data-est-colors.csv > parsed-data-est-colors.csv.clean && mv parsed-data-est-colors.csv.clean parsed-data-est-colors.csv
+
+
 
 SET search_path TO osd, public;
 
--- wrap in a transaction
-BEGIN;
-
 -- create new table for OSD site data
+DROP TABLE IF EXISTS osd.osd_site;
+CREATE TABLE osd.osd_site (
+drainagecl text,
+series citext
+);
 
+\copy osd.osd_site FROM 'parsed-site-data.csv' CSV HEADER NULL 'NA'
+
+-- just in case convert series name to upper case
+UPDATE osd.osd_site SET series = UPPER(series);
+
+-- index
+CREATE INDEX osd_site_series_idx ON osd_site (series);
 
 
 -- create new table that contains colors
 -- load our parsed OSD data into this table
-DROP TABLE osd.osd_colors;
+DROP TABLE IF EXISTS osd.osd_colors;
 CREATE TABLE osd.osd_colors (
 hzname varchar(20),
 top numeric,
@@ -68,20 +83,16 @@ CREATE INDEX osd_on_file_series_idx ON osd.osd_on_file (series);
 COMMIT;
 
 -- permissions
+GRANT SELECT ON osd.osd_site TO soil;
 GRANT SELECT ON osd.osd_colors TO soil;
 GRANT SELECT ON osd.osd_on_file TO soil;
 
-
---
--- 2016-10-03: working on removing all manual fixes from SQL...
--- 
--- manual fixes
---
 
 
 -- update index
 VACUUM ANALYZE osd.osd_on_file;
 VACUUM ANALYZE osd.osd_colors;
+VACUUM ANALYZE osd.osd_site;
 
 -- 
 -- QA/QC: this is done manually
