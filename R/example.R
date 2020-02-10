0042f183a8644c8f6fd67c6ffb50e510e5c5b17a
Dylan Beaudette <debeaudette@ucdavis.edu>
2020-02-10 11:55:18 -0800

    notes

--- R/example.R
+++ R/example.R
@@ -7,6 +7,9 @@ library(aqp)
 
 source('local_functions.R')
 
+# old-style O horizons and formatting errors
+# https://github.com/dylanbeaudette/parse-osd/issues/12
+testIt('KLAWASI')
 
 # 
 testIt('tiger creek')
